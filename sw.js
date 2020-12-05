a3b1e5971ac521a02276d3f7e2db5220bd743acf
Mohamed Elmenoufy <43606746+mohelmenoufy@users.noreply.github.com>
2020-12-05 22:25:41 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,18 +1,4 @@
 self.addEventListener('install', function(e) {
- e.waitUntil(
-   caches.open('fox-store').then(function(cache) {
-     return cache.addAll([
-       '',
-       'index.html',
-       'index.js',
-       'style.css',
-       'images/fox1.jpg',
-       '/images/fox2.jpg',
-       '/images/fox3.jpg',
-       '/images/fox4.jpg'
-     ]);
-   })
- );
 });
 
 self.addEventListener('fetch', function(e) {
