5d53ab341cec7eaf18a4e156aa8b3a276b55b859
Mohamed Elmenoufy <43606746+mohelmenoufy@users.noreply.github.com>
2020-12-05 21:49:33 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ setInterval(function() {
 
 if('serviceWorker' in navigator) {
   navigator.serviceWorker
-           .register('/pwa-examples/a2hs/sw.js')
+           .register('sw.js')
            .then(function() { console.log('Service Worker Registered'); });
 }
 
@@ -34,16 +34,16 @@ window.addEventListener('beforeinstallprompt', (e) => {
   addBtn.style.display = 'block';
 
   addBtn.addEventListener('click', (e) => {
-    // hide our user interface that shows our A2HS button
+    // hide our user interface that shows our button
     addBtn.style.display = 'none';
     // Show the prompt
     deferredPrompt.prompt();
     // Wait for the user to respond to the prompt
     deferredPrompt.userChoice.then((choiceResult) => {
         if (choiceResult.outcome === 'accepted') {
-          console.log('User accepted the A2HS prompt');
+          console.log('User accepted the  prompt');
         } else {
-          console.log('User dismissed the A2HS prompt');
+          console.log('User dismissed the prompt');
         }
         deferredPrompt = null;
       });
